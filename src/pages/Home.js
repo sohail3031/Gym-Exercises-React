dcf57ce6d16688083b940fbe7fdf860f1a4a3e70
sohail3031 <mohdsohailahmed46@gmail.com>
2022-09-17 18:58:27 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
+  console.log("Body Part: ", bodyPart);
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises}
+      />
+    </Box>
+  );
+};
+
+export default Home;
